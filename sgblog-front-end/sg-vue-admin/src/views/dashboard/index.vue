3e2f5e2403396347e0c5298e4006db992d43dc81
itcast <hello@itcast.cn>
2022-11-14 14:48:12 +0800

    blog前端模块

---
+++ sgblog-front-end/sg-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      LJK管理系统
+    </div>
+    <div>
+      欢迎来到LJK的博客！！！
+    </div>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
